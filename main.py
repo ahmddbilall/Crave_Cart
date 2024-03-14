770696f3b25ade6f6707d9c7759e8ccdd8869f6f
bilal <shameerbaba415@gmail.com>
2024-03-14 12:33:36 +0500

    'requirements'

--- main.py
+++ main.py
@@ -2,10 +2,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# USE REQUIREMENTS.txt instead of env
 @app.route('/')
 def index():
     return 'Hello, World!'
+
+#store pdfs in local mechine and in table just store the location of these pdfs. as pdfs are non structural dataset it is not recommended to store pdfs in tables.
 
 if __name__ == '__main__':
     app.run(debug=True,port = 5001)
